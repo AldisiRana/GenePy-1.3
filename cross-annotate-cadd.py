d43831f2429a9a408ff713a992f17189fdaa65c0
Rana Aldisi <35561773+AldisiRana@users.noreply.github.com>
2020-03-13 14:40:57 +0100

    update script to function

--- cross-annotate-cadd.py
+++ cross-annotate-cadd.py
@@ -4,52 +4,30 @@ Created on Wed Oct  2 11:51:25 2019
 
 @author: Enrico
 """
-
-freq = ([line.strip('chr').rstrip().split("\t") for line in open("freqanno","r")])
-cadd = ([line.rstrip().split("\t") for line in open("caddout.tsv","r")])
-
-
-
-cadd.pop(0)
-#cadd.pop(0)
-#freq.pop(0)
-
-F = []
-C = []
-
-for i in freq[1:]:
-    F.append("-".join(i[:2]))
-for i in cadd[1:]:
-    C.append(["-".join(i[:2]),i[-2]])
-
-dC = dict(C)
-#%%
-    
-O = []
-i=0
-for k in F:
-    try:
-        O.append(dC[k])
-    except:
-        i+=1
-        O.append("NAN")
-   
 import numpy as np
-O = np.array(O).astype(np.object)
-
-F =np.array(F)
-
-for i in np.where(O=="NAN")[0]:
-    try:
-        x=F[i]
-        x=x.split("-")
-        x[-1]=str(int(x[-1])-1)
-        x="-".join(x)
-        O[i]=dC[x]
-    except:
-        print('Fail to annotate %s' %(F[i]))
+import pandas as pd
 
 
-out = O[:]
-out = np.insert(out,0,"RawScore")
-np.savetxt("caddanno",out, fmt='%s')
+def cross_annotate_cadd(
+    *,
+    freqanno_file,
+    caddout_file,
+    output_file
+):
+    freq_df = pd.read_csv(freqanno_file, sep='\t', index_col=False)
+    cadd_df = pd.read_csv(caddout_file, sep='\t', skiprows=1, index_col=False)
+    freq = [(val[0], val[1]) for val in freq_df.values]
+    cadd = {
+        (val[0], val[1]): val[-2] for val in cadd_df.values
+    }
+    scores = {}
+    for val in freq:
+        if val not in cadd.keys():
+            val = (val[0], val[1]-1)
+        try:
+            scores[val] = cadd[val]
+        except:
+            scores[val] = 'NaN'
+            print('Fail to annotate ' + str(val))
+    np.savetxt(output_file, ["RawScore"]+list(scores.values()), fmt='%s')
+    return scores
