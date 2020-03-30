2fd0efb7fdfa616142ad63ceac0f17f9b9616ef3
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2020-03-30 12:01:21 +0200

    switch functions from integra to genepy

--- src/genepy/cli.py
+++ src/genepy/cli.py
@@ -9,7 +9,8 @@ import pandas as pd
 from multiprocessing import Pool
 from functools import partial
 
-from .utils import cross_annotate_cadd, calculate_genepy, chunks, run_parallel
+from .utils import cross_annotate_cadd, chunks, run_parallel, find_pvalue, normalize_gene_len, \
+    merge_matrices
 
 
 @click.group()
@@ -90,9 +91,77 @@ def get_genepy(
     return "Scores are ready in " + output_dir
 
 
+@main.command()
+@click.option('-d', '--directory', required=True, help="The directory that contains the matrices to merge.")
+@click.option('-o', '--output-path', required=True, help="The path to output the merged matrix.")
+def merge(
+    *,
+    directory,
+    output_path
+):
+    """This command merges all matrices in a directory into one big matrix"""
+    click.echo("Starting the merging process")
+    merge_matrices(
+        directory=directory,
+        output_path=output_path,
+    )
+    click.echo("Merging is done.")
+
+
+@main.command()
+@click.option('-m', '--matrix-file', required=True, help="The scoring matrix to normalize.")
+@click.option('-g', '--genes-lengths-file',
+              help="The file containing the lengths of genes. If not provided it will be produced.")
+@click.option('-o', '--output-path', required=True, help="The path to output the normalized matrix.")
+def normalize(
+    *,
+    matrix_file,
+    genes_lengths_file=None,
+    output_path=None
+):
+    """This command normalizes the scoring matrix by gene length."""
+    click.echo("Normalization in process.")
+    normalize_gene_len(
+        matrix_file=matrix_file,
+        genes_lengths_file=genes_lengths_file,
+        output_path=output_path,
+    )
+
+
+@main.command()
+@click.option('-s', '--scores-file', required=True, help="The scoring file of genes across a population.")
+@click.option('-i', '--genotype-file', required=True, help="File containing information about the cohort.")
+@click.option('-o', '--output-file', required=True, help="The path to output the pvalues of genes.")
+@click.option('-g', '--genes',
+              help="a list containing the genes to calculate. if not provided all genes will be used.")
+@click.option('-c', '--cases-column', required=True, help="the name of the column that contains the case/control type.")
+def calculate_pval(
+    *,
+    scores_file,
+    genotype_file,
+    output_file,
+    genes,
+    cases_column,
+):
+    click.echo("The process for calculating the p_values will start now.")
+    df = find_pvalue(
+        scores_file=scores_file,
+        output_file=output_file,
+        genotype_file=genotype_file,
+        genes=genes,
+        cases_column=cases_column,
+    )
+    click.echo('Process is complete.')
+    click.echo(df.info())
+
+
 @contextmanager
 def poolcontext(*args, **kwargs):
     pool = Pool(*args, **kwargs)
     yield pool
     pool.terminate()
+
+
+if __name__ == "__main__":
+    main()
 
--- src/genepy/utils.py
+++ src/genepy/utils.py
@@ -6,6 +6,9 @@ import click
 import numpy as np
 import pandas as pd
 import re
+from pybiomart import Dataset
+import scipy.stats as stats
+from tqdm import tqdm
 
 
 def cross_annotate_cadd(
@@ -100,8 +103,109 @@ def run_parallel(header, meta_data, score_col, output_dir, genes):
         gene_df = pd.read_csv(gene+'.meta', sep='\t', index_col=False)
         if gene_df.empty:
             click.echo("Error! Gene not found!")
+            p = subprocess.call(['rm', gene + '.meta'])
             continue
         scores_matrix = calculate_genepy(gene_df, score_col)
         path = os.path.join(output_dir, gene+'_'+score_col+'_matrix')
         np.savetxt(path, scores_matrix, fmt='%s', delimiter='\t')
         p = subprocess.call(['rm', gene+'.meta'])
+
+
+def merge_matrices(
+    *,
+    directory,
+    output_path,
+):
+    """
+    Merges multiple files in a directory, each file should contain the score of a gene across the samples.
+
+    :param directory: the directory that contains files to merge.
+    :param output_path: the path for the merged tsv file.
+    :return: a dataframe combining all information from files.
+    """
+    full_data = pd.DataFrame(data=None, columns=['patient_id'])
+    for filename in tqdm(os.listdir(directory), desc="merging matrices"):
+        data = pd.read_csv(os.path.join(directory, filename), sep='\t',
+                           names=['patient_id', filename.split('_')[0], ''])
+        data = data.drop(columns=[''])
+        full_data = pd.merge(data, full_data, on='patient_id', how='left')
+    full_data.to_csv(output_path, sep='\t', index=False)
+    return full_data
+
+
+def normalize_gene_len(
+    *,
+    genes_lengths_file=None,
+    matrix_file,
+    output_path,
+):
+    """
+    Normalize matrix by gene length.
+
+    :param genes_lengths_file: a file containing genes, and their start and end bps.
+    :param matrix_file: a tsv file containing a matrix of samples and their scores across genes.
+    :param output_path: the path to save the normalized matrix.
+    :return: a normalized dataframe.
+    """
+    if genes_lengths_file:
+        genes_df = pd.read_csv(genes_lengths_file, sep='\t')
+    else:
+        gene_dataset = Dataset(name='hsapiens_gene_ensembl', host='http://www.ensembl.org')
+        genes_df = gene_dataset.query(
+            attributes=['external_gene_name', 'start_position', 'end_position'],
+            only_unique=False,
+        )
+    genes_lengths = {
+        row['Gene name']: round((row['Gene end (bp)'] - row['Gene start (bp)']) / 1000, 3)
+        for _, row in genes_df.iterrows()
+    }
+    scores_df = pd.read_csv(matrix_file, sep='\t')
+    unnormalized = []
+    for (name, data) in tqdm(scores_df.iteritems(), desc="Normalizing genes scores"):
+        if name == 'patient_id':
+            continue
+        if name not in genes_lengths.keys():
+            unnormalized.append(name)
+            continue
+        # normalize genes by length
+        scores_df[name] = round(scores_df[name] / genes_lengths[name], 5)
+    # drop genes with unknown length
+    scores_df = scores_df.drop(unnormalized, axis=1)
+    if output_path:
+        scores_df.to_csv(output_path, sep='\t', index=False)
+    return scores_df
+
+
+def find_pvalue(
+    *,
+    scores_file,
+    genotype_file,
+    output_file,
+    genes=None,
+    cases_column,
+):
+    """
+    Calculate the significance of a gene in a population using Mann-Whitney-U test.
+    :param scores_file: a tsv file containing the scores of genes across samples.
+    :param genotype_file: a file containing the information of the sample.
+    :param output_file: a path to save the output file.
+    :param genes: a list of the genes to calculate the significance. if None will calculate for all genes.
+    :param cases_column: the name of the column containing cases and controls information.
+    :return: dataframe with genes and their p_values
+    """
+    scores_df = pd.read_csv(scores_file, sep='\t', index_col=False)
+    genotype_df = pd.read_csv(genotype_file, sep='\t', index_col=False)
+    merged_df = pd.merge(genotype_df, scores_df, on='patient_id', how='left')
+    df_by_cases = merged_df.groupby(cases_column)
+    cases = list(df_by_cases.groups.keys())
+    p_values = []
+    if genes is None:
+        genes = scores_df.columns.tolist()[1:]
+    for gene in tqdm(genes, desc='Calculating p_values for genes'):
+        case_0 = df_by_cases.get_group(cases[0])[gene].tolist()
+        case_1 = df_by_cases.get_group(cases[1])[gene].tolist()
+        u_statistic, p_val = stats.mannwhitneyu(case_0, case_1)
+        p_values.append([gene, p_val])
+    p_values_df = pd.DataFrame(p_values, columns=['genes', 'p_value']).sort_values(by=['p_value'])
+    p_values_df.to_csv(output_file, sep='\t', index=False)
+    return p_values_df
