0940c7c5623bca4160c97d695a02d77b0dbb1909
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2020-08-03 11:38:07 +0200

    Update cli.py

--- src/genepy/cli.py
+++ src/genepy/cli.py
@@ -161,8 +161,8 @@ def get_genepy_folder(
         processes = processes % 2
         click.echo('processing annotated vcf files')
         func = partial(parallel_annotated_vcf_prcoessing, scores_col, output_path, processes)
-        with threadcontext(processes=processes) as pool:
-            pool.map(func, vcf_files)
+        with threadcontext(processes=processes) as threadpool:
+            threadpool.map(func, vcf_files)
         click.echo('genepy scoring is done.')
     else:
         excluded = output_path + '.excluded'
