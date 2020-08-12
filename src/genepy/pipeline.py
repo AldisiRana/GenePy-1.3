bca1ebd7bafc0a01c85a951459fe883e4fd87ac1
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2020-08-12 11:00:23 +0200

    add beta coef

--- src/genepy/pipeline.py
+++ src/genepy/pipeline.py
@@ -253,10 +253,10 @@ def find_pvalue(
             except:
                 continue
             pval = list(result.pvalues)
-            # add beta coeff
-            p_values.append([gene] + pval)
+            beta_coef = list(result.params)
+            p_values.append([gene] + pval + beta_coef)
         p_values_df = pd.DataFrame(
-            p_values, columns=['genes', 'const_pval', 'p_value', 'PC1_pval', 'PC2_pvcal', 'PC3_pval']
+            p_values, columns=['genes', 'const_pval', 'p_value', 'PC1_pval', 'PC2_pvcal', 'PC3_pval', 'beta_coef']
         ).sort_values(by=['p_value'])
     else:
         raise Exception("The test you selected is not valid.")
